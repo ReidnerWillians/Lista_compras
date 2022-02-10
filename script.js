a3e4b309a13a17f44fc91d4d48cb83bf6ff9b9b7
ReidnerWillians <pacheco195920@gmail.com>
2022-02-09 22:26:37 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const Groceries = document.getElementsByClassName("groceries")[0]
+const Pencil = document.getElementById("pencil")
+const AllItems = document.getElementById("all_Items")
+const UserInput = document.getElementById("user_input")
+
+
