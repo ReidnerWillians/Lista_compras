6c8b159183782d2ea991070e9154ca0679c1da5b
ReidnerWillians <pacheco195920@gmail.com>
2022-02-19 11:18:29 -0300

    update index

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-const Groceries = document.getElementsByClassName("groceries")[0]
-const Pencil = document.getElementById("pencil")
-const AllItems = document.getElementById("all_Items")
-const UserInput = document.getElementById("user_input")
+// const Groceries = document.getElementsByClassName("groceries")[0]
+// const Pencil = document.getElementById("pencil")
+// const AllItems = document.getElementById("all_Items")
+// const UserInput = document.getElementById("user_input")
 
-
+document.querySelector("#pencil").addEventListener("click", () => {
+    document.querySelector("#all_items").textContent = "";
+  })
+  
+  document.querySelector("#user_input").addEventListener("keydown", (event) => {
+    if(event.key == "Enter")
+      addItem();
+  });
+  
+  addItem = () => {
+    const item = document.createElement("h2")
+    item.textContent = "- " + document.querySelector("#user_input").value;
+  
+    item.addEventListener("click", () => {
+      if(item.style.textDecoration != "line-through")
+        item.style.textDecoration = "line-through";
+      else
+        item.style.textDecoration = "none";
+    })
+  
+    document.querySelector("#all_items").appendChild(item);
+    document.querySelector("#user_input").value = "";
+  }
+  
